bafdac62803df11791aad7c10035bd1e10102205
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2021-05-20 02:22:12 +0400

    All apis for Courses

--- core/models.py
+++ core/models.py
@@ -6,14 +6,14 @@ from accounts.models import User
 # Create your models here.
 class Course(models.Model):
     title = models.CharField('Title',max_length=50)
-    techer = models.ForeignKey(User,on_delete=models.CASCADE, db_index=True, related_name='course_teacher')
+    teacher = models.ForeignKey(User,on_delete=models.CASCADE, db_index=True, related_name='course_teacher')
     price = models.DecimalField('Price',max_digits=6, decimal_places=2)
     image = models.ImageField('Image',upload_to='images/')
     description = models.TextField('Description')
     course_deadline = models.IntegerField('Deadline',blank=True)
     minimum_age = models.IntegerField('Minimum age',blank=True,null=False)
     is_shared = models.BooleanField('is shared',default=0)
-    shared_at = models.DateTimeField('shared_at')
+    # shared_at = models.DateTimeField('shared_at')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
