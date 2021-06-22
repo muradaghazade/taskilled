100e4f7369c0e226462b9b529ffc82680c32edae
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2021-06-22 13:08:18 +0400

    m

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -8,11 +8,11 @@ from django.contrib.auth.password_validation import validate_password
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
-    token = serializers.SerializerMethodField(read_only = True)
+    # token = serializers.SerializerMethodField(read_only = True)
     
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'password2','is_student', 'is_teacher' ,'token', 'age')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'password2','is_student', 'is_teacher' ,'age')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
