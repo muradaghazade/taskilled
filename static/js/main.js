e764a3f7afb303c9fa8988cbf21ec1bdae92ba94
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2021-06-24 13:32:58 +0400

    main page done

--- static/js/main.js
+++ static/js/main.js
@@ -61,13 +61,11 @@ getInternshipList = () => {
     courses.slice(0,4).forEach(element => {
         document.querySelector("#fleximg2").innerHTML += `
         
-        <div style="background: url(${element.image}); background-size: cover; background-repeat: no-repeat; height: 200px; width: 300px; border-radius: 5px;">
-          <h3>Price ${element.price}$</h3>
+        <div class="col-md-3 col-12" style="padding: 50px 30px;">
+        <div style="background: url(${element.image}); background-size: cover; background-repeat: no-repeat; height: 350px; padding: 20px; padding-top: 250px; border-radius: 3px;">
+          <h3 style="color: white; font-weight: 200;">Price ${element.price}$</h3>
           <span style="color: white;">${element.title}</span>
-          <img
-            src=""
-            alt=""
-          />
+          </div>
         </div>
       
       
@@ -92,16 +90,22 @@ getBTaskList = () => {
     console.log(document.querySelector("#fleximg3"));
     courses.slice(0,4).forEach(element => {
         document.querySelector("#fleximg3").innerHTML += `
+
+        <div class="col-md-3 mb-3 col-12">
+          <div class="card" style=" height: 350px;">
+          <a href="/course/${element.id}" style="color: black; text-decoration: none;">
+  <div class="card-img-top" style="background: url(${element.image}); height: 170px; background-size: cover;"></div>
+</a>
+
+  <div class="card-body">
+  <h4 class="mt-3">${element.title}</h4>
+  <p>${element.description}</p>
+  <h6 class="card-text">$${element.price}</h6>
+  </div>
+</div>
+          </div>
         
-        <div style="background: url(${element.image}); background-size: cover; background-repeat: no-repeat; height: 200px; width: 300px; border-radius: 5px;">
-          <h3>Price ${element.price}$</h3>
-          <span style="color: white;">${element.title}</span>
-          <img
-            src=""
-            alt=""
-          />
-        </div>
-      
+        
       
         `
     });
