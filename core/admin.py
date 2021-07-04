aa34ad4939919a43be772d81c1af643fd7bb04dd
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2021-07-04 12:36:51 +0400

    m

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register([Course,Question,Option,Subject,UserAnswer,Order,Category])
+admin.site.register([Course,Question,Option,Subject,UserAnswer,Order,Category, AnswerType])
